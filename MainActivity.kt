dda263464d78d1f608b798f99aa90c0937ce38e8
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-22 23:24:57 -0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,32 @@
+/** Copyright (C) Enzo 2025-2026 */
+/** Copyright (C) Enzo inc. 2025-2026 */
+/** Copyright (C) Enzo LLC 2025-2026 */
+val tabelaLetras = mutableListOf<String>()
+/** LETRAS */
+tabelaLetras.add("q")
+tabelaLetras.add("w")
+tabelaLetras.add("e")
+tabelaLetras.add("r")
+tabelaLetras.add("t")
+tabelaLetras.add("y")
+tabelaLetras.add("u")
+tabelaLetras.add("i")
+tabelaLetras.add("o")
+tabelaLetras.add("p")
+tabelaLetras.add("a")
+tabelaLetras.add("s")
+tabelaLetras.add("d")
+tabelaLetras.add("f")
+tabelaLetras.add("g")
+tabelaLetras.add("h")
+tabelaLetras.add("j")
+tabelaLetras.add("k")
+tabelaLetras.add("l")
+tabelaLetras.add("z")
+tabelaLetras.add("x")
+tabelaLetras.add("c")
+tabelaLetras.add("v")
+tabelaLetras.add("b")
+tabelaLetras.add("n")
+tabelaLetras.add("m")
+/** PALAVRAS PT-BR */
