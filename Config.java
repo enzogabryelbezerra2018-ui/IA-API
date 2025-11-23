c9e0774b9db1c98c77c0bfa023fec8b7b28172bc
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-23 00:16:39 -0300

    Update Config.java

--- Config.java
+++ Config.java
@@ -1,1 +1,12 @@
 package com.enzo.util.oficial.app.ia.config;
+import java.util.List;
+import java.util.ArrayList;
+import android.util.Log;
+import com.enzo.outro.Modulo;
+import kotlin.collections.MutableList;
+import java.util.*;
+import kotlin.collections.ArrayList;
+import com.enzo.utils.*;
+int StringMaxMB = 1000;
+int VoxHzMax = 999999999; /** claramente impossível mas isso por causa da não estourar o ouvido da pessoa */
+String VoxIA = "Victor";
