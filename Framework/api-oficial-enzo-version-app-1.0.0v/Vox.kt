6e9af3a8887566a79fa08010864a7d3c27bdb3e8
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-23 01:07:33 -0300

    Update Vox.kt

--- Framework/api-oficial-enzo-version-app-1.0.0v/Vox.kt
+++ Framework/api-oficial-enzo-version-app-1.0.0v/Vox.kt
@@ -1,1 +1,7 @@
 package com.enzo.util.oficial.app.ia.framework.vox
+val comandosIA: MutableMap<String, String> = mutableMapOf()
+
+
+comandosIA["Olá"] = "Oi, como você está?"
+comandosIA["Qual é o seu nome?"] = "Eu sou a IA do Enzo."
+comandosIA["Como você está?"] = "Estou ótima, obrigada por perguntar!"
